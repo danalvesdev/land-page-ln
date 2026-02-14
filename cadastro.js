c7025f5d460647d3a0dd11f0d2941136d5dc4003
Daniel Alves <da445689@gmail.com>
2026-02-14 12:04:58 -0300

    algumas configurações

---
+++ cadastro.js
@@ -0,0 +1,3 @@
+    const paragrafo = document.createElement('p');
+    paragrafo.textContent = 'Este é um parágrafo criado dinamicamente com JavaScript. Você pode escrever qualquer mensagem aqui!';
+    document.body.appendChild(paragrafo);
