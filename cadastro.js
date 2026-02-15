3cedc9412503af7539eda3fa66a4fb5e8b618e64
Daniel Alves <da445689@gmail.com>
2026-02-15 18:13:54 -0300

    nada feito

--- cadastro.js
+++
@@ -1,3 +0,0 @@
-    const paragrafo = document.createElement('p');
-    paragrafo.textContent = 'Este é um parágrafo criado dinamicamente com JavaScript. Você pode escrever qualquer mensagem aqui!';
-    document.body.appendChild(paragrafo);
---
+++ click.js
@@ -0,0 +1,9 @@
+function clickMenu(){
+if (conteiner.style.display == 'block'){
+        conteiner.style.display = 'none'
+    }
+
+else {
+    conteiner.style.display = 'block'
+    }
+}
